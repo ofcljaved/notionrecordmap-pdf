27aaffd141c64c5a7732d8656f11078f4d92e710
Javed <javedofficialme@gmail.com>
2024-03-29 07:25:34 +0530

    feat: Pdf generation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import fs from 'fs/promises';
 import { renderer } from './renderer';
+import puppeteer from 'puppeteer';
 
 (async function getContent() {
   try {
@@ -10,8 +11,31 @@ import { renderer } from './renderer';
     const data = JSON.parse(rawData);
     const javed = renderer({ recordMap: data, fullPage: true, darkMode: true });
     const formatted = javed?.replace(/\s*(<[^>]+>)\s*/g, '$1');
+    const style = await fs.readFile('./src/style.css', { encoding: 'utf-8' });
+    const htmlContent = `
+<!DOCTYPE html>
+<html lang="en">
+<body class="notion-app dark-mode">
+  <style>
+    ${style};
+  </style>
+  ${formatted}
+</body>
+</html>
+`;
 
-    console.log(formatted);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.setContent(htmlContent);
+
+    await page.pdf({
+      path: 'example.pdf',
+      format: 'A4',
+      margin: { top: 0 },
+      printBackground: true,
+    });
+    await browser.close();
   } catch (error) {
     console.log('--------Shit happens\n', error);
   }
