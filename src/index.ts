1870bf51b420c60cc956fd9d74fd8aa96e11918a
Javed <javedofficialme@gmail.com>
2024-04-14 17:10:22 +0530

    chore: build project

--- src/index.ts
+++ src/index.ts
@@ -1,111 +1,114 @@
 import fs from 'fs/promises';
+import path from 'path';
 import { renderer } from './renderer';
 import puppeteer from 'puppeteer';
 import Handlebars from 'handlebars';
+import { ExtendedRecordMap } from './types';
 
-// import express from 'express';
+// (async function getContent() {
+//   // console.log(path.join(__dirname, 'style.css'));
 
-// const app = express();
-
-// app.get('/', (_, res) => {
-//   res.send('WORKING JUST FINE');
-// });
-
-// app.get('/pdf', async (_, res) => {
 //   try {
 //     const rawData = await fs.readFile(
-//       './src/assets/recordmap.org.json',
-//       'utf-8'
+//       path.join(__dirname, 'assets', 'recordmap.org.json'),
+//       { encoding: 'utf-8' }
 //     );
 //     const data = JSON.parse(rawData);
-//     const javed = renderer({ recordMap: data, fullPage: true, darkMode: true });
-//     const formatted = javed?.replace(/\s*(<[^>]+>)\s*/g, '$1');
-//     const style = await fs.readFile('./src/style.css', { encoding: 'utf-8' });
-//     const htmlContent = `
-// <!DOCTYPE html>
-// <html lang="en">
-// <body class="notion-app dark-mode">
-//   <style>
-//     ${style};
-//   </style>
-//   ${formatted}
-// </body>
-// </html>
-// `;
+
+//     let notionPage = '';
+
+//     data.forEach((d: any) => {
+//       notionPage += renderer({
+//         recordMap: d,
+//         fullPage: true,
+//         darkMode: true,
+//       });
+//     });
+//     const cssPath = path.join(__dirname, 'style.css');
+//     const style = await fs.readFile(cssPath, { encoding: 'utf-8' });
+//     const htmlSource = `<!DOCTYPE html>
+//     <html lang="en">
+//     <body class="notion-app dark-mode">
+//       <style>{{style}}</style>
+//       {{{notionPage}}}
+//     </body>
+//     </html>
+//     `;
+//     const htmlTemplate = Handlebars.compile(htmlSource);
+//     const htmlContent = htmlTemplate({ notionPage, style });
 //     const browser = await puppeteer.launch();
 //     const page = await browser.newPage();
 
 //     await page.setContent(htmlContent);
 
-//     const pdf = await page.pdf({
+//     await page.pdf({
+//       path: 'example.pdf',
 //       format: 'A4',
+//       margin: { top: 0 },
 //       printBackground: true,
 //     });
 //     await browser.close();
-//     res.setHeader('Content-Type', 'application/pdf');
-//     res.send(pdf);
 //   } catch (error) {
 //     console.log('--------Shit happens\n', error);
-//     res.send('ERROR-------');
 //   }
-// });
+// })();
 
-// app.listen(8080, () => {
-//   console.log('SERVER IS RUNNING ON PORT: 8080');
-// });
-
-(async function getContent() {
+async function renderContent({
+  data,
+  fullPage,
+  darkMode,
+}: {
+  data: ExtendedRecordMap[];
+  fullPage: boolean;
+  darkMode: boolean;
+}) {
   try {
-    const rawData = await fs.readFile(
-      './src/assets/recordmap.org.json',
-      'utf-8'
-    );
-    const data = JSON.parse(rawData);
-
-    console.log(Array.isArray(data));
-
     let notionPage = '';
-
-    if (Array.isArray(data)) {
-      data.forEach(d => {
-        notionPage += renderer({
-          recordMap: d,
-          fullPage: true,
-          darkMode: true,
-        });
+    data.forEach(d => {
+      notionPage += renderer({
+        recordMap: d,
+        fullPage,
+        darkMode,
       });
-    } else {
-      notionPage = renderer({
-        recordMap: data,
-        fullPage: true,
-        darkMode: true,
-      });
-    }
-    const style = await fs.readFile('./src/style.css', { encoding: 'utf-8' });
+    });
+    const cssPath = path.join(__dirname, 'style.css');
+    const style = await fs.readFile(cssPath, { encoding: 'utf-8' });
     const htmlSource = `<!DOCTYPE html>
-    <html lang="en">
-    <body class="notion-app dark-mode">
-      <style>{{style}}</style>
-      {{{notionPage}}}
-    </body>
-    </html>
-    `;
+      <html lang="en">
+      <body class="notion-app dark-mode">
+        <style>{{style}}</style>
+        {{{notionPage}}}
+      </body>
+      </html>
+      `;
     const htmlTemplate = Handlebars.compile(htmlSource);
     const htmlContent = htmlTemplate({ notionPage, style });
+    return htmlContent;
+  } catch (error) {
+    return '';
+  }
+}
 
+export async function generatePdf(props: {
+  data: ExtendedRecordMap[];
+  fullPage: boolean;
+  darkMode: boolean;
+}) {
+  try {
+    const htmlContent = await renderContent(props);
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     await page.setContent(htmlContent);
 
-    await page.pdf({
-      path: 'example.pdf',
+    const pdf = await page.pdf({
       format: 'A4',
       margin: { top: 0 },
       printBackground: true,
     });
     await browser.close();
+    return pdf;
   } catch (error) {
-    console.log('--------Shit happens\n', error);
+    return null;
   }
-})();
+}
